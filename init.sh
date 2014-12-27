7a794c8e31f00097761315b36f3c8f2790a0508c
Vaibhav Gupta <vaibhavtheory@gmail.com>
2014-12-27 13:29:55 -0600

    yelp dataset challenge

---
+++ init.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+python parser.py dataset/yelp_academic_dataset_business.json
+python parser.py dataset/yelp_academic_dataset_checkin.json
+python parser.py dataset/yelp_academic_dataset_review.json
+python parser.py dataset/yelp_academic_dataset_tip.json
+python parser.py dataset/yelp_academic_dataset_user.json

d5581b39b5d43b222c4a8b9d5dc5c46323736449
Vaibhav Gupta <vaibhavtheory@gmail.com>
2014-12-28 15:20:08 -0600

    parsing complete, able to get all restaurants and their bucket values in one file

--- program/system.py
+++ program/system.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 import argparse
+from data import r, c, Data, get_file_name
+import os.path
 
 # final program
 # input: [lat, lng, category, distance]
@@ -16,31 +18,44 @@ def score(buisness_id):
 def train(buisnesses_list = []):
   pass
 
-def main():
-  pass
+def init():
+  f = get_file_name('review_data')
+  if os.path.isfile(f):
+    r.read()
+  else:
+    r.fill()
+    r.write()
+
+  f = get_file_name('checkin_data')
+  if os.path.isfile(f):
+    c.read()
+  else:
+    c.fill()
+    c.write()
+
+def main(lat, lng, dist):
+  d = Data({'lat': lat, 'lng': lng, 'dist': dist})
+  ending = '-' + str(lat) + '-' + str(lng) + '-' + str(dist)
+  f = get_file_name('business' + ending)
+  if os.path.isfile(f):
+    d.read(ending)
+  else:
+    print "Loading... this may take a while"
+    init()
+    d.fill()
+    d.write()
+
+  content = d.content
+  print len(content.keys())
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(
-            description='Determine relation between categories',
-            )
+  parser = argparse.ArgumentParser(description='Determine relation between categories')
 
-    parser.add_argument(
-            'latitude',
-            type=float,
-            help='',
-            )
+  parser.add_argument('latitude',type=float,help='',)
 
-    parser.add_argument(
-            'longitude',
-            type=float,
-            help='',
-            )
+  parser.add_argument('longitude',type=float,help='',)
 
-    parser.add_argument(
-            'distance',
-            type=float,
-            help='',
-            )
+  parser.add_argument('distance',type=float,help='',)
 
-    args = parser.parse_args()
-  main()
+  args = parser.parse_args()
+  main(args.latitude, args.longitude, args.distance)
